c71ccc276c58ff7d4dbf6d927da32c6895e13746
Michał Wielich <mwielich@unamo.com>
2017-10-10 17:15:01 +0200

    minor refactor

--- lib/valid_url.rb
+++ lib/valid_url.rb
@@ -26,8 +26,8 @@ module ActiveModel
       protected
 
       # add common protocol by default
-      def ensure_protocol url
-        if url[/\A(http|https):\/\//i]
+      def ensure_protocol(url)
+        if url[/\A(#{PROTOCOLS.join('|')}):\/\//i]
           url
         else
           "http://" + url
@@ -35,43 +35,43 @@ module ActiveModel
       end
 
       # http and https are accepted
-      def valid_scheme? scheme
+      def valid_scheme?(scheme)
         UrlValidator::PROTOCOLS.include?(scheme.mb_chars.downcase.to_s)
       end
 
-      def valid_host? host
+      def valid_host?(host)
         return false unless host.present? && valid_characters?(host)
         labels = host.split('.')
         valid_length?(host) && valid_labels?(labels) && (valid_ip?(host) || valid_zone?(labels.last))
       end
 
       # each label must be between 1 and 63 characters long
-      def valid_labels? labels
+      def valid_labels?(labels)
         labels.count >= 2 && labels.all?{ |label| label.length >= 1 && label.length <= 63 }
       end
 
       # entire hostname has a maximum of 253 characters
-      def valid_length? host
+      def valid_length?(host)
         host.length <= 253
       end
 
       # only existent domain name zones
-      def valid_zone? zone
+      def valid_zone?(zone)
         UrlValidator::ZONES.include?(zone.mb_chars.downcase.to_s)
       end
 
       # check if host is an ip-address
-      def valid_ip? host
+      def valid_ip?(host)
         host =~ Resolv::IPv4::Regex
       end
 
       # disallow some prohibited characters
-      def valid_characters? host
+      def valid_characters?(host)
         !host[/[\s\!\\"$%&'\(\)*+_,:;<=>?@\[\]^|£§°ç\/]/] && host.last != '.'
       end
 
       # disallow blank caracters in the path
-      def valid_path? path
+      def valid_path?(path)
         !path[/\s/]
       end
 
